fa41fd47d25ae07dc77c36f494fda048c085b29c
Seungjun Eo <eoseungjun7@naver.com>
2023-10-12 16:18:20 +0900

    Env: 라우팅 설정 + recoil 폴더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Home } from 'pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactㅋ
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/Final-Project" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,1 +1,11 @@
-export {};
+import React, { useState } from 'react';
+
+export const Home: React.FC = () => {
+  console.log('ㅋㅋ');
+  return (
+    <>
+      <div>ㅋㅋ</div>
+      <div>ㅋㅋ</div>
+    </>
+  );
+};
