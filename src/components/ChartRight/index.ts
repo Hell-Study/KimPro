8d46a12a1b8a7cee37a90698c62f4c0926898aa5
Seungjun Eo <eoseungjun7@naver.com>
2023-10-16 14:12:01 +0900

    Feat: lightweight-charts 구현

--- src/components/ChartRight/index.ts
+++ src/components/ChartRight/index.ts
@@ -1,1 +1,2 @@
-export {};
+import ChartRight from './ChartRight';
+export { ChartRight };
--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import { Table } from 'components/Table';
+import { ChartRight } from 'components/ChartRight';
+import { ChartLeft } from 'components/ChartLeft';
 import { useFetchMarketCode } from 'use-upbit-api';
 import { marketCodesState } from 'recoil/atoms/upbit';
 import { DisplayBoard } from './Home.styles';
+import styled from 'styled-components';
+
+const ChartsWrapper = styled.div`
+  background-color: whitesmoke;
+  padding: 5px;
+  display: grid;
+  gap: 5px;
+  grid-template-columns: 1fr 1fr;
+`;
 
 export const Home: React.FC = () => {
   const { isLoading, marketCodes: fetchedMC } = useFetchMarketCode();
@@ -18,6 +29,10 @@ export const Home: React.FC = () => {
 
   return (
     <DisplayBoard>
+      <ChartsWrapper>
+        <ChartLeft />
+        <ChartRight />
+      </ChartsWrapper>
       <Table />
     </DisplayBoard>
   );
