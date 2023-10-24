7892e16ffd83fb976bd1005e5173ba16f9406dd0
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-10-24 17:37:18 +0900

    Feat: 다크모드 toggle 버튼 구현

--- src/components/common/Header/Header.styles.ts
+++ src/components/common/Header/Header.styles.ts
@@ -4,10 +4,13 @@ interface RateProps {
   $isPositive: boolean;
 }
 
-export const HeaderContainer = styled.div`
+export const HeaderContainer = styled.header`
   position: sticky;
   top: 0;
   z-index: 2;
+`;
+
+export const Topbar = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -18,4 +21,29 @@ export const HeaderContainer = styled.div`
 
 export const Rate = styled.span<RateProps>`
   color: ${(props) => (props.$isPositive ? 'red' : 'green')};
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 100px;
+  height: 8vh;
+  background-color: ${({ theme }) => theme.colors.bg_element4};
+`;
+
+export const SwitchButton = styled.button<{ $isDarkMode: boolean }>`
+  font-size: 1.25rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.icon};
+  font-size: ${({ $isDarkMode }) => ($isDarkMode ? '1.2rem' : '1.4rem')};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:focus {
+    outline: none;
+  }
 `;
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import * as styled from './Header.styles';
 import getGlobalCoinData from 'api/getGlobalCoinData';
 import { useRecoilState } from 'recoil';
 import { globalCoinState } from 'recoil/atoms/globalCoin';
+import { themeState } from 'recoil/atoms/theme';
+import { HiSun, HiMoon } from 'react-icons/hi2';
+import { SwitchButton } from './Header.styles';
 
 function Header() {
   const [globalCoin, setGlobalCoin] = useRecoilState(globalCoinState);
+  const [theme, setTheme] = useRecoilState(themeState);
+
+  const isDarkMode = theme === 'dark';
+  const handleToggle = () => {
+    setTheme(isDarkMode ? 'light' : 'dark');
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,54 +31,62 @@ function Header() {
 
   return (
     <styled.HeaderContainer>
-      <div>김프사이트</div>
-      <div>환율 : xx</div>
-      {globalCoin && (
-        <>
-          <div>
-            암호화폐 :
-            {globalCoin[0]?.coins_count
-              ? globalCoin[0].coins_count.toString()
-              : '로딩 중...'}
-          </div>
-          <div>
-            거래소 :
-            {globalCoin[0]?.active_markets
-              ? globalCoin[0].active_markets.toString()
-              : '로딩 중...'}
-          </div>
-          <div>
-            시가총액 :
-            {globalCoin[0]?.total_mcap
-              ? `$${globalCoin[0].total_mcap.toString()}`
-              : '로딩 중...'}
-            <styled.Rate $isPositive={globalCoin[0]?.mcap_change >= 0}>
-              {globalCoin[0]?.mcap_change !== undefined
-                ? (globalCoin[0].mcap_change >= 0 ? '+' : '-') +
-                  Math.abs(globalCoin[0].mcap_change).toString() +
-                  '%'
+      <styled.Topbar>
+        <div>김프사이트</div>
+        <div>환율 : xx</div>
+        {globalCoin && (
+          <>
+            <div>
+              암호화폐 :
+              {globalCoin[0]?.coins_count
+                ? globalCoin[0].coins_count.toString()
                 : '로딩 중...'}
-            </styled.Rate>
-          </div>
-          <div>
-            24시간 거래량 :
-            {globalCoin[0]?.total_volume
-              ? `$${globalCoin[0].total_volume.toString()}`
-              : '로딩 중...'}
-            <styled.Rate $isPositive={globalCoin[0]?.volume_change >= 0}>
-              {globalCoin[0]?.volume_change !== undefined
-                ? (globalCoin[0].volume_change >= 0 ? '+' : '-') +
-                  Math.abs(globalCoin[0].volume_change).toString() +
-                  '%'
+            </div>
+            <div>
+              거래소 :
+              {globalCoin[0]?.active_markets
+                ? globalCoin[0].active_markets.toString()
                 : '로딩 중...'}
-            </styled.Rate>
-          </div>
-          <div>
-            BTC 점유율 :
-            {globalCoin[0]?.btc_d ? `${globalCoin[0].btc_d}%` : '로딩 중...'}
-          </div>
-        </>
-      )}
+            </div>
+            <div>
+              시가총액 :
+              {globalCoin[0]?.total_mcap
+                ? `$${globalCoin[0].total_mcap.toString()}`
+                : '로딩 중...'}
+              <styled.Rate $isPositive={globalCoin[0]?.mcap_change >= 0}>
+                {globalCoin[0]?.mcap_change !== undefined
+                  ? (globalCoin[0].mcap_change >= 0 ? '+' : '-') +
+                    Math.abs(globalCoin[0].mcap_change).toString() +
+                    '%'
+                  : '로딩 중...'}
+              </styled.Rate>
+            </div>
+            <div>
+              24시간 거래량 :
+              {globalCoin[0]?.total_volume
+                ? `$${globalCoin[0].total_volume.toString()}`
+                : '로딩 중...'}
+              <styled.Rate $isPositive={globalCoin[0]?.volume_change >= 0}>
+                {globalCoin[0]?.volume_change !== undefined
+                  ? (globalCoin[0].volume_change >= 0 ? '+' : '-') +
+                    Math.abs(globalCoin[0].volume_change).toString() +
+                    '%'
+                  : '로딩 중...'}
+              </styled.Rate>
+            </div>
+            <div>
+              BTC 점유율 :
+              {globalCoin[0]?.btc_d ? `${globalCoin[0].btc_d}%` : '로딩 중...'}
+            </div>
+          </>
+        )}
+      </styled.Topbar>
+      <styled.HeaderWrapper>
+        <h1>로고</h1>
+        <SwitchButton $isDarkMode={isDarkMode} onClick={handleToggle}>
+          {isDarkMode ? <HiMoon /> : <HiSun />}
+        </SwitchButton>
+      </styled.HeaderWrapper>
     </styled.HeaderContainer>
   );
 }
