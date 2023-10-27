f14d230a9b604b3a7eae47bc216e3fe0a8d8a76f
jisu Seo <jseo9732@gmail.com>
2023-10-27 14:33:26 +0900

    Feat: 테이블 정렬 기능(빗썸) 구현

--- src/components/Table/Bithumb.tsx
+++ src/components/Table/Bithumb.tsx
@@ -6,6 +6,8 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { coingeckoCoinDataState } from 'recoil/atoms/coingecko';
 import { getCoingeckoData } from 'api/coingecko/getCoingeckoData';
 import useBinanceTicker from 'hooks/binance/useBinanceTicker';
+import { tableSortUpDownState, tableSortValueState } from 'recoil/atoms/table';
+import { changesRatio, highRatio, lowRatio } from 'utils/bithumbCalc';
 
 export function Bithumb() {
   const socketDatas = useBithumbWsTicker();
@@ -21,6 +23,84 @@ export function Bithumb() {
   const removeUSDT = (symbol: string) => {
     return symbol.replace('USDT', '');
   };
+
+  const tableSortValue = useRecoilValue(tableSortValueState);
+  const tableSortUpDown = useRecoilValue(tableSortUpDownState);
+  useEffect(() => {
+    switch (tableSortValue) {
+      case '코인':
+        if (tableSortUpDown) {
+          socketDatas.sort((a, b) => {
+            if (a[0] > b[0]) {
+              return 1;
+            } else {
+              return -1;
+            }
+          });
+        } else {
+          socketDatas.sort((a, b) => {
+            if (a[0] > b[0]) {
+              return -1;
+            } else {
+              return 1;
+            }
+          });
+        }
+        break;
+      case '현재가':
+        if (tableSortUpDown) {
+          socketDatas.sort(
+            (a, b) => Number(a[1].closing_price) - Number(b[1].closing_price),
+          );
+        } else {
+          socketDatas.sort(
+            (a, b) => Number(b[1].closing_price) - Number(a[1].closing_price),
+          );
+        }
+        break;
+      // case '김프':
+      //   socketDatas.sort(
+      //     (a, b) => a.acc_trade_price_24h - b.acc_trade_price_24h,
+      //   );
+      //   break;
+      case '전일대비':
+        if (tableSortUpDown) {
+          socketDatas.sort((a, b) => changesRatio(a) - changesRatio(b));
+        } else {
+          socketDatas.sort((a, b) => changesRatio(b) - changesRatio(a));
+        }
+        break;
+      case '고가대비(전일)':
+        if (tableSortUpDown) {
+          socketDatas.sort((a, b) => highRatio(a) - highRatio(b));
+        } else {
+          socketDatas.sort((a, b) => highRatio(b) - highRatio(a));
+        }
+        break;
+      case '저가대비(전일)':
+        if (tableSortUpDown) {
+          socketDatas.sort((a, b) => lowRatio(a) - lowRatio(b));
+        } else {
+          socketDatas.sort((a, b) => lowRatio(b) - lowRatio(a));
+        }
+        break;
+      case '거래액(일)':
+        if (tableSortUpDown) {
+          socketDatas.sort(
+            (a, b) =>
+              Number(a[1].acc_trade_value_24H) -
+              Number(b[1].acc_trade_value_24H),
+          );
+        } else {
+          socketDatas.sort(
+            (a, b) =>
+              Number(b[1].acc_trade_value_24H) -
+              Number(a[1].acc_trade_value_24H),
+          );
+        }
+        break;
+    }
+  }, [socketDatas, tableSortValue, tableSortUpDown]);
 
   return (
     <styled.CoinListWrapper>
