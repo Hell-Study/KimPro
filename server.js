7ff17634e991934a47e38b42621dad6d96690032
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-02 16:24:58 +0900

    Comment: gitignore 작성

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 8080;
-
-app.use('/Final-Project', express.static('build'));
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/Final-Project`);
-});
