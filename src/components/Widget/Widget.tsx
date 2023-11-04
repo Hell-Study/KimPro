c004f8a35716278e8b24fafe70313f2443e8b0ba
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-04 21:20:28 +0900

    Refactor: Widget 컴포넌트 memo & suspense 적용

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useWidgetTickers } from 'hooks';
 import { PAIR_DATA } from './Widget.constants';
 import { ChartWidget } from './ChartWidget';
@@ -6,24 +6,31 @@ import { TickerWidget } from './TickerWidget';
 import { Carousel } from './Carousel';
 import * as styled from './Widget.styles';
 
-export const Widget: React.FC = () => {
+export const Widget: React.FC = React.memo(() => {
   return (
-    <Carousel slidesToShow={4}>
-      {Object.entries(PAIR_DATA).map(([key, { id }]) => {
-        const { data: baseData } = useWidgetTickers(id, 'P1D', 'previous');
-        if (!baseData) return null;
-        return (
-          <styled.Card key={key}>
-            <styled.TickerRow>
-              <styled.Ticker>{key}</styled.Ticker>
-              <TickerWidget pairId={id} baseData={baseData} />
-            </styled.TickerRow>
-            <styled.ChartRow>
-              <ChartWidget pairId={id} baseData={baseData} />
-            </styled.ChartRow>
-          </styled.Card>
-        );
-      })}
-    </Carousel>
+    <Suspense fallback={<styled.SkeletonCard />}>
+      <Carousel slidesToShow={4}>
+        {Object.entries(PAIR_DATA).map(([ticker, { id }]) => {
+          const { data: baseData } = useWidgetTickers(id, 'P1D', 'previous');
+          if (!baseData) return null;
+          return (
+            <styled.Card key={ticker}>
+              <styled.TickerRow>
+                <styled.Ticker>{ticker}</styled.Ticker>
+                <Suspense fallback={<styled.SkeletonTicker />}>
+                  <TickerWidget pairId={id} baseData={baseData} />
+                </Suspense>
+              </styled.TickerRow>
+              <styled.ChartRow>
+                <Suspense fallback={<styled.SkeletonChart />}>
+                  <ChartWidget pairId={id} baseData={baseData} />
+                </Suspense>
+              </styled.ChartRow>
+            </styled.Card>
+          );
+        })}
+      </Carousel>
+    </Suspense>
   );
-};
+});
+Widget.displayName = 'Widget';
